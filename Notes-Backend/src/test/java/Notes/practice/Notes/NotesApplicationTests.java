43a400dc198031746188b420161bd0dee5794340
Muaz Asim <110489042+muazasim@users.noreply.github.com>
2024-06-13 18:13:16 +0500

    Springboot boiler code with dependencies

---
+++ Notes-Backend/src/test/java/Notes/practice/Notes/NotesApplicationTests.java
@@ -0,0 +1,13 @@
+package Notes.practice.Notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
